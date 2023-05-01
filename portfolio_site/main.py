de0e4414a43c2f1cbef581044597b697b69ff67d
spacemanspiff-713 <87157301+spacemanspiff-713@users.noreply.github.com>
2023-05-01 13:08:33 -0500

    Create columns, profile image, and description

--- portfolio_site/main.py
+++ portfolio_site/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
 
+with col1:
+    st.image("images/profile-pic.png")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with col2:
+    st.title("Jason Trimble")
+    content = """
+    Hello, World! I'm Jason, a python developer. Check out my projects and reach out to discuss partnering together for any projects you have in mind.
+    """
+    st.info(content)
