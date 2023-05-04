6e5f06fc5e497602252f03707c3b92bb9f131fbb
spacemanspiff-713 <87157301+spacemanspiff-713@users.noreply.github.com>
2023-05-04 15:22:24 -0500

    add project columns and details

--- portfolio_site/main.py
+++ portfolio_site/main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas
+
+df = pandas.read_csv("data.csv", sep=";")
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -16,3 +19,19 @@ with col2:
 contact_info = "Below you can find my current projects 👇🏼"
 
 st.write(contact_info)
+
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
